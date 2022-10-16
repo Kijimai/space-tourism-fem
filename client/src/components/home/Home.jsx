9d85b65b47f3ba53a237b8f876a4312010ff3c09
John Deen Bucog <JDBucog@yahoo.com>
2022-10-16 14:29:35 -0700

    minor responsiveness

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -2,19 +2,27 @@ import React from "react"
 
 const Home = () => {
   return (
-    <div className="container flex">
-      <h1>
-        So, you want to travel to <span>Space</span>
-      </h1>
-      Let’s face it; if you want to go to space, you might as well genuinely go
-      to outer space and not hover kind of on the edge of it. Well sit back, and
-      relax because we’ll give you a truly out of this world experience!
-      <a
-        href="#"
-        className="bg-white large-button uppercase text-dark ff-serif fs-600"
-      >
-        Explore
-      </a>
+    <div className="grid-container">
+      <div>
+        <h1 className="uppercase text-light-blue fs-500 letter-spacing-1">
+          So, you want to travel to
+          <span className="fs-900 ff-serif text-white">Space</span>
+        </h1>
+        <p>
+          Let’s face it; if you want to go to space, you might as well genuinely
+          go to outer space and not hover kind of on the edge of it. Well sit
+          back, and relax because we’ll give you a truly out of this world
+          experience!
+        </p>
+      </div>
+      <div>
+        <a
+          href="#"
+          className="bg-white large-button uppercase text-dark ff-serif fs-600"
+        >
+          Explore
+        </a>
+      </div>
     </div>
   )
 }
